8101483203361df6b14428257cfa59fa3878c9f3
Parham Alvani <parham.alvani@gmail.com>
2020-01-08 21:58:36 +0330

    feat: Complete a Working Example for Kafka Soccer Events

--- src/main/java/home/parham/jflink/StreamingJob.java
+++ src/main/java/home/parham/jflink/StreamingJob.java
@@ -18,11 +18,19 @@
 
 package home.parham.jflink;
 
-import home.parham.jflink.op.Splitter;
+import home.parham.jflink.domain.SoccerEvent;
+import home.parham.jflink.marshal.SoccerEventSchema;
+import home.parham.jflink.op.SoccerEventTypeCounter;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.util.Collector;
+
+import java.util.Properties;
 
 /**
  * Skeleton for a Flink Streaming Job.
@@ -41,16 +49,29 @@ public class StreamingJob {
 	public static void main(String[] args) throws Exception {
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+		Properties kafkaProperties = new Properties();
+		kafkaProperties.setProperty("bootstrap.servers", "localhost:9092");
+		kafkaProperties.setProperty("group.id", "sevda");
+
 		DataStream<Tuple2<String, Integer>> dataStream = env
-				.socketTextStream("localhost", 9999)
-				.flatMap(new Splitter())
-				.keyBy(0)
-				.timeWindow(Time.seconds(5))
+				.addSource(new FlinkKafkaConsumer<>("events", new SimpleStringSchema(), kafkaProperties))
+                .flatMap(new FlatMapFunction<String, SoccerEvent>() {
+					@Override
+					public void flatMap(String value, Collector<SoccerEvent> out) throws Exception {
+						out.collect(new SoccerEventSchema().deserialize(value));
+					}
+				})
+				.flatMap(new SoccerEventTypeCounter())
+                .keyBy(0)
+                .timeWindow(Time.seconds(60))
 				.sum(1);
 
-		dataStream.print();
-		dataStream.writeToSocket("localhost", 9998, element -> element.toString().getBytes());
 
-		env.execute("Window WordCount");
+		dataStream.print();
+		// uncomment the following line to have output on socket. please note that if you want to have output on socket
+		// you must run the listener before submitting the task.
+		// dataStream.writeToSocket("localhost", 9998, element -> element.toString().getBytes());
+
+		env.execute("Soccer Events Processor!");
 	}
 }
