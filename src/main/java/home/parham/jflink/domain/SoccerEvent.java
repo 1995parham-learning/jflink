bccb737e2402ee197b0a3ae6fe01d40d221a0dfb
Parham Alvani <parham.alvani@gmail.com>
2020-01-08 21:58:11 +0330

    fix: Correct Soccer Events Structure

--- src/main/java/home/parham/jflink/domain/SoccerEvent.java
+++ src/main/java/home/parham/jflink/domain/SoccerEvent.java
@@ -3,29 +3,7 @@ package home.parham.jflink.domain;
 import java.sql.Timestamp;
 
 public class SoccerEvent {
-    private Timestamp timestamp;
-    private SoccerEventType type;
-    private int team;
-
-    public SoccerEvent() {
-
-    }
-
-    public SoccerEvent(Timestamp timestamp, SoccerEventType type, int team) {
-        this.timestamp = timestamp;
-        this.type = type;
-        this.team = team;
-    }
-
-    public int getTeam() {
-        return team;
-    }
-
-    public Timestamp getTimestamp() {
-        return timestamp;
-    }
-
-    public SoccerEventType getType() {
-        return type;
-    }
+    public Timestamp timestamp;
+    public SoccerEventType type;
+    public int team;
 }
--- src/main/java/home/parham/jflink/domain/SoccerEventType.java
+++ src/main/java/home/parham/jflink/domain/SoccerEventType.java
@@ -1,11 +1,20 @@
 package home.parham.jflink.domain;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum SoccerEventType {
+    @SerializedName("Goal")
     Goal,
-    Penalty ,
+    @SerializedName("Penalty")
+    Penalty,
+    @SerializedName("Offside")
     Offside,
+    @SerializedName("Corner")
     Corner,
+    @SerializedName("FreeKick")
     FreeKick,
+    @SerializedName("Assist")
     Assist,
+    @SerializedName("Foul")
     Foul,
 }
