a1dd553d27a7b53c6827f7788f7819c559ff8f73
Parham Alvani <parham.alvani@gmail.com>
2020-01-09 08:15:46 +0330

    fix: Remove Debug Log

--- src/main/java/home/parham/jflink/marshal/SoccerEventSchema.java
+++ src/main/java/home/parham/jflink/marshal/SoccerEventSchema.java
@@ -5,10 +5,8 @@ import home.parham.jflink.domain.SoccerEvent;
 
 public class SoccerEventSchema {
     public SoccerEvent deserialize(String message) {
-        System.out.println(message);
         Gson gson = new Gson();
         SoccerEvent event = gson.fromJson(message, SoccerEvent.class);
-        System.out.println(event);
         return event;
     }
 }
