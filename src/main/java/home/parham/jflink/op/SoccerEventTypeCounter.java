477381a24cf0ae22fa5cce7133cb48c1d3220d25
Parham Alvani <parham.alvani@gmail.com>
2020-01-09 07:37:32 +0330

    fix: Remove Debug Log

--- src/main/java/home/parham/jflink/op/SoccerEventTypeCounter.java
+++ src/main/java/home/parham/jflink/op/SoccerEventTypeCounter.java
@@ -8,9 +8,6 @@ import org.apache.flink.util.Collector;
 public class SoccerEventTypeCounter implements FlatMapFunction<SoccerEvent, Tuple2<String, Integer>> {
     @Override
     public void flatMap(SoccerEvent value, Collector<Tuple2<String, Integer>> out) throws Exception {
-        System.out.println(value.team);
-        System.out.println(value.timestamp);
-        System.out.println(value.type);
         out.collect(new Tuple2<>(value.type.name(), 1));
     }
 }
